7d60bf0c1ec7a4f517054fd4dd31d6776fb4f106
xtreme-sort <duggiralayashaswi147@gmail.com>
2025-04-09 05:06:03 +0000

    Corrected MEX

--- array/mex.cpp
+++ array/mex.cpp
@@ -12,17 +12,18 @@ void array_input(T_vector &v, int n) {
 
 int main(){
     int N;
-    cin>>N;
+    cin >> N;
 
     vector<int> V(N);
-    array_input(V,N);
+    array_input(V, N);
 
     auto MEX = [&](vector<int> V) -> int {
-        sort(V.begin(),V.end());
+        sort(V.begin(), V.end());
         
         int mex = 0;
         for(int i = 0 ; i < N ; i++){
             if(V[i] == mex) mex++;
+            else if(i > 0 and V[i] == V[i-1]) continue;
             else return i;
         }
 
